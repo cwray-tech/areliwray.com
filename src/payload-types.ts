a99906180ec1e947d93423e99eb98b40c4442719
Christopher Wray <chris@sol.company>
2026-02-17 07:43:00 -0800

    update revalidateTag calls to match updated parameters in latest next version

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -114,9 +114,7 @@ export interface Config {
     footer: FooterSelect<false> | FooterSelect<true>;
   };
   locale: null;
-  user: User & {
-    collection: 'users';
-  };
+  user: User;
   jobs: {
     tasks: {
       schedulePublish: TaskSchedulePublish;
@@ -411,6 +409,7 @@ export interface User {
       }[]
     | null;
   password?: string | null;
+  collection: 'users';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
