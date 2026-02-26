dd0f53dd8fa165bcf0233d698be383885de96c2d
Christopher Wray <chris@sol.company>
2026-02-26 00:48:21 -0800

    add post as valid prop type in link component

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,4 +1,4 @@
-import type { Page, Product } from '@/payload-types'
+import type { Page, Post, Product } from '@/payload-types'
 
 import { Button, type ButtonProps } from '@/components/ui/button'
 import { cn } from '@/utilities/cn'
@@ -12,8 +12,8 @@ type CMSLinkType = {
   label?: string | null
   newTab?: boolean | null
   reference?: {
-    relationTo: 'pages' | 'posts'
-    value: Page | Product | string | number
+    relationTo: 'pages' | 'posts' | 'products'
+    value: Page | Product | Post | string | number
   } | null
   size?: ButtonProps['size'] | null
   type?: 'custom' | 'reference' | null
