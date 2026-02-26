fdb395ddc1c692b94890c0a53f8c3957f7a99a84
Christopher Wray <chris@sol.company>
2026-02-26 01:16:50 -0800

    fix type errors on CartModal

--- src/components/Cart/CartModal.tsx
+++ src/components/Cart/CartModal.tsx
@@ -20,7 +20,8 @@ import { DeleteItemButton } from './DeleteItemButton'
 import { EditItemQuantityButton } from './EditItemQuantityButton'
 import { OpenCartButton } from './OpenCart'
 import { Button } from '@/components/ui/button'
-import { Product } from '@/payload-types'
+import { Product, VariantOption } from '@/payload-types'
+import { CartItem } from '.'
 
 export function CartModal() {
   const { cart } = useCart()
@@ -85,14 +86,12 @@ export function CartModal() {
                   if (isVariant) {
                     price = variant?.priceInUSD
 
-                    const imageVariant = product.gallery?.find((item) => {
-                      if (!item.variantOption) return false
+                    const imageVariant = product.gallery?.find((item: CartItem) => {
+                      if (!item.variant) return false
                       const variantOptionID =
-                        typeof item.variantOption === 'object'
-                          ? item.variantOption.id
-                          : item.variantOption
+                        typeof item.variant === 'object' ? item.variant.id : item.variant
 
-                      const hasMatch = variant?.options?.some((option) => {
+                      const hasMatch = variant?.options?.some((option: VariantOption) => {
                         if (typeof option === 'object') return option.id === variantOptionID
                         else return option === variantOptionID
                       })
@@ -132,7 +131,7 @@ export function CartModal() {
                             {isVariant && variant ? (
                               <p className="text-sm text-neutral-500 dark:text-neutral-400 capitalize">
                                 {variant.options
-                                  ?.map((option) => {
+                                  ?.map((option: VariantOption) => {
                                     if (typeof option === 'object') return option.label
                                     return null
                                   })
