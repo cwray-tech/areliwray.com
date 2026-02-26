0ad2c02dbbc8290054e382ddc26e72095377d278
Christopher Wray <chris@sol.company>
2026-02-26 01:14:21 -0800

    fix type errors with metadata

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -4,7 +4,9 @@ import type { Page, Post, Product } from '../payload-types'
 
 import { mergeOpenGraph } from './mergeOpenGraph'
 
-export const generateMeta = async (args: { doc: Page | Product | Post }): Promise<Metadata> => {
+export const generateMeta = async (args: {
+  doc: Page | Product | Post | null
+}): Promise<Metadata> => {
   const { doc } = args || {}
 
   const ogImage =
