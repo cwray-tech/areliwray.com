0203b327c016fbb306aed6fda287241c9d3b72b4
Christopher Wray <chris@sol.company>
2026-02-26 01:28:48 -0800

    add additional css variables from ecommerce template

--- src/cssVariables.js
+++ src/cssVariables.js
@@ -8,5 +8,16 @@ export const cssVariables = {
     lg: 1024,
     md: 768,
     sm: 640,
+    l: 1440,
+    m: 1024,
+    s: 768,
+  },
+  colors: {
+    base0: 'rgb(255, 255, 255)',
+    base100: 'rgb(235, 235, 235)',
+    base500: 'rgb(128, 128, 128)',
+    base850: 'rgb(34, 34, 34)',
+    base1000: 'rgb(0, 0, 0)',
+    error500: 'rgb(255, 111, 118)',
   },
 }
